75fc0de52da4dcbeb2994a0e960ec87333da6fa6
thflf <thflf93@gmail.com>
2023-12-05 17:33:21 +0900

    feat: join 기능 추가

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,17 +1,45 @@
 package com.ll.medium.domain.member.member.controller;
 
+import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.service.MemberService;
+import com.ll.medium.global.rq.Rq;
+import com.ll.medium.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final Rq rq;
 
     @GetMapping("/member/login")
     String showLogin() {
         return "domain/member/member/login";
+    }
+
+    @GetMapping("/member/join")
+    String showJoin() {
+        return "domain/member/member/join";
+    }
+
+    @Data
+    public static class JoinForm {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
+    @PostMapping("/member/join")
+    String join(@Valid JoinForm joinForm){
+        RsData<Member> joinRs = memberService.join(joinForm.username, joinForm.password);
+
+        return rq.redirectOrBack("/member/login", joinRs);
     }
 }
--- src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
+++ src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
@@ -1,11 +1,49 @@
 package com.ll.medium.domain.member.member.service;
 
+import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.repository.MemberRepository;
+import com.ll.medium.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public RsData<Member> join(String username, String password) {
+        if (findByUsername(username).isPresent()) {
+            return new RsData<>("F-1", "이미 사용중인 아이디입니다.");
+        }
+
+        password = passwordEncoder.encode(password);
+        Member member = Member
+                .builder()
+                .username(username)
+                .password(password)
+                .build();
+
+        memberRepository.save(member);
+
+        return new RsData<>(
+                "S-1",
+                "%s님 환영합니다.".formatted(member.getUsername()),
+                member
+        );
+    }
+
+    public Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+
+    public long count() {
+        return memberRepository.count();
+    }
 }
