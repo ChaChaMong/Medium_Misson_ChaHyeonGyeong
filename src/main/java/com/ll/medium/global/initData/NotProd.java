764209b45b29693d27d13f5021c9dd2795c3792b
thflf <thflf93@gmail.com>
2023-12-05 17:34:39 +0900

    feat: pre dev data 추가

---
+++ src/main/java/com/ll/medium/global/initData/NotProd.java
@@ -0,0 +1,38 @@
+package com.ll.medium.global.initData;
+
+import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.member.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
+
+@Profile("!prod")
+@Configuration
+@RequiredArgsConstructor
+public class NotProd {
+    @Autowired
+    @Lazy
+    private NotProd self;
+    private final MemberService memberService;
+
+    @Bean
+    public ApplicationRunner initNotProdData() {
+        return args -> {
+            self.work1();
+        };
+    }
+
+    @Transactional
+    public void work1() {
+        if (memberService.count() > 0) return;
+
+        Member member1 = memberService.join("user1", "1234").getData();
+        Member member2 = memberService.join("user2", "1234").getData();
+        Member member3 = memberService.join("user3", "1234").getData();
+    }
+}
