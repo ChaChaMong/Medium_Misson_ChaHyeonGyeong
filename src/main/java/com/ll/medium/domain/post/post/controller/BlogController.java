fd234f010f1788b9747d858106b36452a1a6b87d
thflf <thflf93@gmail.com>
2023-12-11 14:31:40 +0900

    feat: 특정 회원의 전체 글 리스트 조회 기능

---
+++ src/main/java/com/ll/medium/domain/post/post/controller/BlogController.java
@@ -0,0 +1,31 @@
+package com.ll.medium.domain.post.post.controller;
+
+import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.member.member.service.MemberService;
+import com.ll.medium.domain.post.post.entity.Post;
+import com.ll.medium.domain.post.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BlogController {
+    private final PostService postService;
+    private final MemberService memberService;
+
+    @GetMapping("/b/{username}")
+    public String showListByUsername(Model model, @PathVariable String username) {
+        Member member = memberService.findByUsername(username).get();
+        List<Post> posts = postService.findByAuthorIdOrderByIdDesc(member.getId());
+        model.addAttribute("username", username);
+        model.addAttribute("posts", posts);
+
+        return "domain/post/blog/list";
+    }
+
+}
