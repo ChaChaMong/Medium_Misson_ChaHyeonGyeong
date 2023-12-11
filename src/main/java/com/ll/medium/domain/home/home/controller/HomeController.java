168d1293e5cedefbeb14315a780201b800b82c0e
thflf <thflf93@gmail.com>
2023-12-11 18:27:01 +0900

    refactor: 불필요한 변수 제거

--- src/main/java/com/ll/medium/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/medium/domain/home/home/controller/HomeController.java
@@ -2,7 +2,6 @@ package com.ll.medium.domain.home.home.controller;
 
 import com.ll.medium.domain.post.post.entity.Post;
 import com.ll.medium.domain.post.post.service.PostService;
-import com.ll.medium.global.rq.Rq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +12,6 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-    private final Rq rq;
     private final PostService postService;
 
     @GetMapping("/")
