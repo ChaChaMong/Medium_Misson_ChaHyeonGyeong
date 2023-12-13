64bcd0a8a0e00b9ca32b4b1739f6001d01f814b6
thflf <thflf93@gmail.com>
2023-12-13 21:57:36 +0900

    feat: post controller restapi로 구현

---
+++ src/main/java/com/ll/medium/global/security/ApiSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.ll.medium.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class ApiSecurityConfig {
+    @Bean
+    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/api/**")
+                .csrf(AbstractHttpConfigurer::disable)
+        ;
+
+        return http.build();
+    }
+}
