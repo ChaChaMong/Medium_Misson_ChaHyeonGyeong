8a70baedca71a298f33d3f63a0c0b528d519c2b6
ChaChaMong <124334963+ChaChaMong@users.noreply.github.com>
2023-12-17 01:43:22 +0900

    Revert "Merge First Branch" (#3)
    This reverts commit b78205f5d0d8159103da1ea1829a8778651f8987.

--- src/main/java/com/ll/medium/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/medium/domain/home/home/controller/HomeController.java
@@ -1,25 +1,14 @@
 package com.ll.medium.domain.home.home.controller;
 
-import com.ll.medium.domain.post.post.entity.Post;
-import com.ll.medium.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-    private final PostService postService;
-
     @GetMapping("/")
-    public String showMain(Model model) {
-        List<Post> posts = postService.findTop30ByIsPublishedOrderByIdDesc(true);
-        model.addAttribute("paging", posts);
-        model.addAttribute("detailUrl", "/post");
-
+    public String showMain() {
         return "domain/home/home/main";
     }
 }
