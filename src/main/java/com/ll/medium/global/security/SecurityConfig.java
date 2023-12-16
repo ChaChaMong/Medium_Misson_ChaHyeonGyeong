eb45d2cb88fb61c21d0aab7a15fabd8f977874d4
ChaChaMong <124334963+ChaChaMong@users.noreply.github.com>
2023-12-17 00:03:38 +0900

    Revert "Merge First Branch"
    This reverts commit b78205f5d0d8159103da1ea1829a8778651f8987.

--- src/main/java/com/ll/medium/global/security/SecurityConfig.java
+++ src/main/java/com/ll/medium/global/security/SecurityConfig.java
@@ -2,52 +2,32 @@ package com.ll.medium.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
-@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(
-                        csrf -> csrf
-                                .ignoringRequestMatchers(
-                                        "/h2-console/**"
-                                )
-                )
-                .headers(
-                        headers -> headers
-                                .addHeaderWriter(
-                                        new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)
-                                )
-                )
-                .authorizeHttpRequests(
-                        authorizeHttpRequests -> authorizeHttpRequests
-                                .requestMatchers("/**")
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests.requestMatchers("/**")
                                 .permitAll()
                 )
-                .formLogin(
-                        formLogin -> formLogin
-                                .loginPage("/member/login")
-                                .defaultSuccessUrl("/")
+                .headers(
+                        headers ->
+                                headers.frameOptions(
+                                        frameOptions ->
+                                                frameOptions.sameOrigin()
+                                )
                 )
-                .logout(
-                        logout -> logout
-                                .logoutUrl("/member/logout")
-                                .logoutSuccessUrl("/")
+                .csrf(
+                        csrf ->
+                                csrf.ignoringRequestMatchers(
+                                        "/h2-console/**"
+                                )
                 );
 
         return http.build();
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
