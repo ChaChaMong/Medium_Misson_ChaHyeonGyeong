4506e31e509b53ff06bf7861227c0c61d8d6da95
thflf <thflf93@gmail.com>
2023-12-27 15:44:08 +0900

    refactor: config 파일 수정

--- src/main/java/com/ll/medium/global/app/AppConfig.java
+++ src/main/java/com/ll/medium/global/app/AppConfig.java
@@ -9,42 +9,47 @@ import java.io.IOException;
 
 @Configuration
 public class AppConfig {
-    private static String resourcesStaticDirPath;
-
-    @Getter
-    public static String tempDirPath;
-
-    @Getter
-    public static String genFileDirPath;
-
-    @Getter
-    public static String siteName;
-
-    @Getter
-    public static String siteBaseUrl;
-
     @Getter
     public static String jwtSecretKey;
 
-    @Value("${custom.tempDirPath}")
-    public void setTempDirPath(String tempDirPath) {
-        AppConfig.tempDirPath = tempDirPath;
+    @Value("${custom.jwt.secretKey}")
+    public void setJwtSecretKey(String jwtSecretKey) {
+        this.jwtSecretKey = jwtSecretKey;
     }
 
-    @Value("${custom.genFile.dirPath}")
-    public void setGenFileDirPath(String genFileDirPath) {
-        AppConfig.genFileDirPath = genFileDirPath;
+    @Getter
+    private static long accessTokenExpirationSec;
+
+    @Value("${custom.accessToken.expirationSec}")
+    public void setJwtSecretKey(long accessTokenExpirationSec) {
+        this.accessTokenExpirationSec = accessTokenExpirationSec;
     }
 
-    @Value("${custom.site.name}")
-    public void setSiteName(String siteName) {
-        AppConfig.siteName = siteName;
+    @Getter
+    private static String siteFrontUrl;
+
+    @Value("${custom.site.frontUrl}")
+    public void setSiteFrontUrl(String siteFrontUrl) {
+        this.siteFrontUrl = siteFrontUrl;
     }
 
-    @Value("${custom.site.baseUrl}")
-    public void setSiteBaseUrl(String siteBaseUrl) {
-        AppConfig.siteBaseUrl = siteBaseUrl;
+    @Getter
+    private static String siteBackUrl;
+
+    @Value("${custom.site.backUrl}")
+    public void setSiteBackUrl(String siteBackUrl) {
+        this.siteBackUrl = siteBackUrl;
     }
+
+    @Getter
+    private static String siteCookieDomain;
+
+    @Value("${custom.site.cookieDomain}")
+    public void setSiteCookieDomain(String siteCookieDomain) {
+        this.siteCookieDomain = siteCookieDomain;
+    }
+
+    private static String resourcesStaticDirPath;
 
     public static String getResourcesStaticDirPath() {
         if (resourcesStaticDirPath == null) {
@@ -59,8 +64,27 @@ public class AppConfig {
         return resourcesStaticDirPath;
     }
 
-    @Value("${custom.jwt.secretKey}")
-    public void setJwtSecretKey(String jwtSecretKey) {
-        AppConfig.jwtSecretKey = jwtSecretKey;
+    @Getter
+    public static String tempDirPath;
+
+    @Value("${custom.temp.dirPath}")
+    public void setTempDirPath(String tempDirPath) {
+        AppConfig.tempDirPath = tempDirPath;
+    }
+
+    @Getter
+    public static String genFileDirPath;
+
+    @Value("${custom.genFile.dirPath}")
+    public void setGenFileDirPath(String genFileDirPath) {
+        AppConfig.genFileDirPath = genFileDirPath;
+    }
+
+    @Getter
+    public static String siteName;
+
+    @Value("${custom.site.name}")
+    public void setSiteName(String name) {
+        AppConfig.siteName = name;
     }
 }
