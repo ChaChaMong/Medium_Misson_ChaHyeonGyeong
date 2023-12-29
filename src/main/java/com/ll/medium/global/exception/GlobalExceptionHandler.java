8c9e0f2640583bd8a9c6011854f6c53ba0be6cd2
thflf <thflf93@gmail.com>
2023-12-30 01:25:14 +0900

    refactor: exceptionHandler restapi용으로 변경

--- src/main/java/com/ll/medium/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ll/medium/global/exception/GlobalExceptionHandler.java
@@ -6,10 +6,10 @@ import com.ll.medium.standard.base.Empty;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<RsData<Empty>> handleResourceNotFoundException(ResourceNotFoundException e) {
